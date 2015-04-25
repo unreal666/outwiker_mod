15a01c1ceddaac626fe8f9ac974762c2e95862a5
unreal666 <vit.space.gol@gmail.com>
2015-04-26 08:16:50 +1000

    В wiki-токен таблицы добавил подтокен для генерации "заголовочной строки" (<thead><tr><th>текст</th><th>текст</th></tr></thead>)

--- src/outwiker/pages/wiki/parser/tokentable.py
+++ src/outwiker/pages/wiki/parser/tokentable.py
@@ -20,16 +20,53 @@ class TableToken (object):
 
 
     def getToken (self):
-        tableCell = Regex (r"(?P<text>(.|(\\\n))*?)(\\\n\s*)*\|\|", re.UNICODE)
+        tableHeaderCell = Regex (r"(?P<text>(.|(\\\n))*?)(\\\n\s*)*\|\|", re.UNICODE)
+        tableHeaderCell.setParseAction(self.__convertTableHeaderCell)
+
+        tableHeaderRow = LineStart() + "|||" + OneOrMore (tableHeaderCell) + LineEnd()
+        tableHeaderRow.setParseAction(self.__convertTableHeaderRow)
+
+        tableCell = tableHeaderCell.copy()
         tableCell.setParseAction(self.__convertTableCell)
 
         tableRow = LineStart() + "||" + OneOrMore (tableCell) + Optional (LineEnd())
         tableRow.setParseAction(self.__convertTableRow)
 
-        table = LineStart() + Regex (r"\|\| *(?P<params>.+)?", re.UNICODE) + LineEnd() + OneOrMore (tableRow)
+        table = LineStart() + Regex (r"\|\| *(?P<params>.+)?", re.UNICODE) + LineEnd() + Optional (tableHeaderRow) + OneOrMore (tableRow)
         table = table.setParseAction(self.__convertTable)("table")
 
         return table
+
+
+    def __convertTableHeaderCell (self, s, loc, toks):
+        text = toks["text"]
+
+        leftAlign = toks["text"][-1] in " \t"
+
+        # Условие в скобках связано с тем, что первый пробел попадает
+        # или не попадает в токен в зависимости от того, первая ячейка в строке или нет
+        rightAlign = loc > 0 and (s[loc - 1] in " \t" or s[loc] in " \t")
+
+        align = u''
+
+        if leftAlign and rightAlign:
+            align = u' align="center"'
+        elif leftAlign:
+            align = u' align="left"'
+        elif rightAlign:
+            align = u' align="right"'
+
+        result = u'<th%s>%s</th>' % (align, self.parser.parseWikiMarkup (text.strip()))
+
+        return result
+
+
+    def __convertTableHeaderRow (self, s, l, t):
+        result = u"<thead><tr>"
+        for element in t[1: len (t) - 1]:
+            result += element
+
+        return result + "</tr></thead>"
 
 
     def __convertTableCell (self, s, loc, toks):
