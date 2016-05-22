c61d2782b7907773175e0258f073206cf25d9116
unreal666 <vit.space.gol@gmail.com>
2016-05-22 22:36:40 +1000

    Исправлен шаблон в методе parseParams класса Command.

--- src/outwiker/pages/wiki/parser/command.py
+++ src/outwiker/pages/wiki/parser/command.py
@@ -43,7 +43,7 @@ class Command (object):
 
             Changes in 1.9.0.761: name may contain a dot.
         """
-        pattern = ur"""((?P<name>[-a-zA-Z_.]+)
+        pattern = ur"""((?P<name>[-a-zA-Z0-9_.]+)
     (\s*=\s*(?P<param>([-_\w.]+)|((?P<quote>["']).*?(?P=quote)) ) )?\s*)"""
 
         result = {}
