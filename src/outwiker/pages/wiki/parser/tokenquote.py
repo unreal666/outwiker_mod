8d9dba4f1705899029a074cb4b24f6fe0a047051
unreal666 <vit.space.gol@gmail.com>
2017-01-08 16:26:25 +1000

    Fixed QuoteToken

--- src/outwiker/pages/wiki/parser/tokenquote.py
+++ src/outwiker/pages/wiki/parser/tokenquote.py
@@ -2,6 +2,7 @@
 
 from outwiker.libs.pyparsing import Forward, CharsNotIn, NotAny, ZeroOrMore, OneOrMore, Combine, Literal, Suppress, Regex
 from .utils import TagAttrsPattern, getAttributes
+import sys
 
 
 class QuoteFactory (object):
@@ -16,38 +17,27 @@ class QuoteFactory (object):
 class QuoteToken (object):
     quoteStart = '[>'
     quoteEnd = '<]'
-    anyExcept = Combine(ZeroOrMore(NotAny (Literal(quoteStart) | Literal(quoteEnd)) + CharsNotIn('', exact=1)))
 
     def __init__ (self, parser):
         self.parser = parser
 
 
     def getToken (self):
+        anyExcept = Combine(ZeroOrMore(NotAny (Literal(QuoteToken.quoteStart) | Literal(QuoteToken.quoteEnd)) + CharsNotIn('', exact=1)))
+        anyExcept = anyExcept.leaveWhitespace().setParseAction(self.__parseText)
         token = Forward()
         token << (Suppress(QuoteToken.quoteStart) + Regex(TagAttrsPattern.value) +
-                  (OneOrMore(QuoteToken.anyExcept + token) +
-                   QuoteToken.anyExcept | QuoteToken.anyExcept) +
+                  (OneOrMore(anyExcept + token) + anyExcept | anyExcept) +
                   Suppress(QuoteToken.quoteEnd)).leaveWhitespace().setParseAction(self.__parse)("quote")
         return token
 
 
     def __parse (self, s, loc, toks):
         text = u''.join(toks[1:len(toks)])
-        leftpos = text.find (u'<blockquote>')
-        rightpos = text.rfind (u'</blockquote>')
-
         attrs = getAttributes(toks)
 
-        if leftpos == -1 or rightpos == -1:
-            return u''.join([u'<blockquote', attrs, u'>', self.parser.parseWikiMarkup (text), u'</blockquote>'])
+        return u''.join([u'<blockquote', attrs, u'>', text, u'</blockquote>'])
 
-        lefttext = text[:leftpos]
-        righttext = text[rightpos:]
-        centertext = text[leftpos:rightpos]
 
-        return u''.join([
-            u'<blockquote>',
-            self.parser.parseWikiMarkup (lefttext),
-            centertext,
-            self.parser.parseWikiMarkup (righttext),
-            u'</blockquote>'])
+    def __parseText (self, s, loc, toks):
+        return self.parser.parseWikiMarkup (u''.join(toks))
