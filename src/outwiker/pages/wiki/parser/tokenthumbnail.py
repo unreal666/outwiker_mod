f171e654d3cde331637568ada9bd21b4e054b509
unreal666 <vit.space.gol@gmail.com>
2019-08-23 15:39:35 +1000

    Рефакторинг ThumbnailToken

--- src/outwiker/pages/wiki/parser/tokenthumbnail.py
+++ src/outwiker/pages/wiki/parser/tokenthumbnail.py
@@ -46,56 +46,51 @@ class ThumbnailToken(object):
                            \s*%\s*
                            Attach:(?P<fname>.*?\.(?:gif|png|jpe?g|bmp|tiff?|webp|svg))\s*%%""",
                            re.IGNORECASE | re.VERBOSE)
-        result = result.setParseAction(self.__convertThumb)("thumbnail")
+        result = result.setParseAction(self.__convertThumb)('thumbnail')
         return result
 
     def __convertThumb(self, s, l, t):
-        if t["soft"]:
-            unit = t["unit"] or 'px'
-            if not t["nolink"]:
-                template = '<a href="{0}/{1}"><img src="{0}/{1}" style="{4}:{2}{3}"/></a>'
-                template2 = '<a href="{0}/{1}"><img src="{0}/{1}" style="max-width:{2}{3};max-height:{2}{3}"/></a>'
-                template3 = '<a href="{0}/{1}"><img src="{0}/{1}" style="width:{2}{3};height:{4}{5}"/></a>'
-            else:
-                template = '<img src="{0}/{1}" style="{4}:{2}{3}"/>'
-                template2 = '<img src="{0}/{1}" style="max-width:{2}{3};max-height:{2}{3}"/>'
-                template3 = '<img src="{0}/{1}" style="width:{2}{3};height:{4}{5}"/>'
-        else:
-            template = '<img src="{0}"/>' if t["nolink"] else '<a href="{1}/{2}"><img src="{0}"/></a>'
+        nolink = t['nolink']
 
-        fname = t["fname"]
+        if t['soft']:
+            unit = t['unit'] or 'px'
 
-        if t["width"] is not None:
-            size = self.__convertSize(t["width"])
+        fname = t['fname']
 
-            if t["soft"]:
-                unit_w = t["unit_w"] or 'px'
+        if t['width'] is not None:
+            size = self.__convertSize(t['width'])
 
-                if t["height_w"] is not None:
-                    height = self.__convertSize(t["height_w"])
-                    return template3.format(PAGE_ATTACH_DIR, fname, size, unit_w, height, unit)
+            if t['soft']:
+                unit_w = t['unit_w'] or 'px'
 
-                return template.format(PAGE_ATTACH_DIR, fname, size, unit_w, "width")
+                if t['height_w'] is None:
+                    return self.__getSoftResult(nolink, fname, size, unit_w, 'width')
+
+                return self.__getSoftResult(
+                    nolink, fname, size, unit_w,
+                    self.__convertSize(t['height_w']), unit)
+
+                return self.__getSoftResult(nolink, fname, size, unit_w, 'width')
 
             func = self.thumbmaker.createThumbByWidth
 
-        elif t["height"] is not None:
-            size = self.__convertSize(t["height"])
+        elif t['height'] is not None:
+            size = self.__convertSize(t['height'])
 
-            if t["soft"]:
-                return template.format(PAGE_ATTACH_DIR, fname, size, unit, "height")
+            if t['soft']:
+                return self.__getSoftResult(nolink, fname, size, unit, 'height')
 
             func = self.thumbmaker.createThumbByHeight
 
         else:
-            if t["maxsize"] is not None:
-                size = self.__convertSize(t["maxsize"])
+            if t['maxsize'] is not None:
+                size = self.__convertSize(t['maxsize'])
             else:
                 config = WikiConfig(self.parser.config)
                 size = config.thumbSizeOptions.value
 
-            if t["soft"]:
-                return template2.format(PAGE_ATTACH_DIR, fname, size, unit)
+            if t['soft']:
+                return self.__getSoftResult(nolink, fname, size, unit)
 
             func = self.thumbmaker.createThumbByMaxSize
 
@@ -105,10 +100,26 @@ class ThumbnailToken(object):
         except (ThumbException, IOError):
             return _("<b>Can't create thumbnail for \"{}\"</b>").format(fname)
 
-        return template.format(thumb.replace("\\", "/"), PAGE_ATTACH_DIR, fname)
+        template = '<img src="{0}"/>' if t['nolink'] else '<a href="{1}/{2}"><img src="{0}"/></a>'
+        return template.format(thumb.replace('\\', '/'), PAGE_ATTACH_DIR, fname)
 
-    def __convertSize(self, text):
-        float_value = float(text)
+    def __convertSize(self, value):
+        float_value = float(value)
         int_value = int(float_value)
 
         return int_value if float_value == int_value else float_value
+
+    def __getSoftResult(self, nolink, *args):
+        args_len = len(args)
+
+        if args_len == 3:
+            template = '<img src="{0}/{1}" style="max-width:{2}{3};max-height:{2}{3}"/>'
+        elif args_len == 4:
+            template = '<img src="{0}/{1}" style="{4}:{2}{3}"/>'
+        else: # 5 аргументов
+            template = '<img src="{0}/{1}" style="width:{2}{3};height:{4}{5}"/>'
+
+        if not nolink:
+            template = '<a href="{0}/{1}">%s</a>' % template
+
+        return template.format(PAGE_ATTACH_DIR, *args)
