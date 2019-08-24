a96d34039b848c900c29c2d0653ec4556dff7160
unreal666 <vit.space.gol@gmail.com>
2019-08-25 07:37:46 +1000

    В UrlToken добавлена поддержка отключениня автопреобразования ссылок

--- src/outwiker/pages/wiki/parser/tokenurl.py
+++ src/outwiker/pages/wiki/parser/tokenurl.py
@@ -2,7 +2,7 @@
 
 import re
 
-from outwiker.libs.pyparsing import Regex
+from outwiker.libs.pyparsing import Regex, NoMatch
 
 
 class UrlFactory (object):
@@ -14,27 +14,25 @@ class UrlFactory (object):
 class UrlToken (object):
     def __init__(self, parser):
         self.parser = parser
+        if not hasattr(parser, 'customProps'): parser.customProps = {}
+        parser.customProps.setdefault('parsing', {}).setdefault('tokenurl', True)
 
     def getToken(self):
-        token = Regex(
-            r'((?# Начало разбора IP )(?<!\.)(?:25[0-5]|2[0-4]\d|1\d\d|0?[1-9]\d|0{,2}[1-9])(?:\.(?:25[0-5]|2[0-4]\d|[01]?\d?\d)){3}(?!\.[0-9])(?!\w)(?# Конец разбора IP )|(((news|telnet|nttp|file|http|ftp|https|page)://)|(www|ftp)\.)[-\w0-9\.]+[-\w0-9]+)(:[0-9]*)?(/([-\w0-9_,\$\.\+\!\*\(\):@|&=\?/~\#\%]*[-\w0-9_\$\+\!\*\(\):@|&=\?/~\#\%])?)?', re.IGNORECASE)("url")
+        if self.parser.customProps['parsing']['tokenurl'] is not False:
+            token = Regex(
+            r'((?# Начало разбора IP )(?<!\.)(?:25[0-5]|2[0-4]\d|1\d\d|0?[1-9]\d|0{,2}[1-9])(?:\.(?:25[0-5]|2[0-4]\d|[01]?\d?\d)){3}(?!\.[0-9])(?!\w)(?# Конец разбора IP )|(((news|telnet|nttp|file|https?|gopher|ftp|page)://)|(www|ftp)\.)[-\w0-9\.]+[-\w0-9]+)(:[0-9]*)?(/([-\w0-9_,\$\.\+\!\*\(\):@|&=\?/~\#\%]*[-\w0-9_\$\+\!\*\(\):@|&=\?/~\#\%])?)?', re.IGNORECASE)("url")
 
-        token.setParseAction(self.__convertToUrlLink)
+            token.setParseAction(self.__convertToUrlLink)
+        else:
+            token = NoMatch()
+
         return token
 
     def __convertToUrlLink(self, s, l, t):
         """
         Преобразовать ссылку на интернет-адрес
         """
-        if (not t[0].startswith("http://") and
-                not t[0].startswith("ftp://") and
-                not t[0].startswith("news://") and
-                not t[0].startswith("gopher://") and
-                not t[0].startswith("telnet://") and
-                not t[0].startswith("nttp://") and
-                not t[0].startswith("file://") and
-                not t[0].startswith("https://") and
-                not t[0].startswith("page://")):
+        if not re.search('^(?:https?|ftp|news|gopher|telnet|nttp|file|page)://', t[0]):
             return self.__getUrlTag("http://" + t[0], t[0])
 
         return self.__getUrlTag(t[0], t[0])
